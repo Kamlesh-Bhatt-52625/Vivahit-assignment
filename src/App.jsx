ce5b98b61fd5a48470d99a02ffbab0faf6d0e0d0
Kamlesh Bhatt <kb55262555@gmail.com>
2024-03-08 17:19:21 +0530

    Initial setup done with vite + react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Common/Header";
+
+function App() {
+  return (
+    <div className=''>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Common/Header/index.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className='w-full text-white bg-black '>index</div>;
+};
+
+export default Header;
